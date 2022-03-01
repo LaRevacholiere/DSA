ae33ac1bd32e79c6b5089d1b4ad60f91114483e1
catch the rainbow <60229017+LaRevacholiere@users.noreply.github.com>
2022-03-01 18:59:08 -0500

    Update quickSort.java
    Take median of three as the pivot

--- quickSort.java
+++ quickSort.java
@@ -1,30 +1,42 @@
 public class quickSort {
+    static int partition(int[] arr, int start, int end) {
+        int m = medianOfThree(arr, start, end);
+        int pivot = arr[m];
+        swap(arr, m, end);
+        int i = start, j = start;
+        while (j < end) {
+            if (arr[j] < pivot) {
+                swap(arr, i, j);
+                i++;
+            }
+            j++;
+        }
+        swap(arr, i, end);
+        return i;
+    }
+
+    static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pi = partition(arr, start, end);
+            quickSort(arr, start, pi - 1);
+            quickSort(arr, pi + 1, end);
+        }
+    }
+
+    static int medianOfThree(int[] arr, int start, int end) {
+        int mid = start+(end-start)/2;
+        if ((arr[start] < arr[mid] && arr[mid] < arr[end]) || (arr[end] < arr[mid] && arr[mid] < arr[start]))
+            return mid;
+        else if ((arr[mid] < arr[start] && arr[start] < arr[end]) || (arr[end] < arr[start] && arr[start] < arr[mid]))
+            return start;
+        else
+            return end;
+    }
+
     static void swap(int[] arr, int i, int j) {
         int temp = arr[i];
         arr[i] = arr[j];
         arr[j] = temp;
-    }
-
-    //takes last element as pivot
-    static int partition(int[] arr, int low, int high) {
-        int pivot = arr[high];
-        int i = (low - 1);
-        for(int j = low; j < high; j++) {
-            if (arr[j] < pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-        swap(arr, i + 1, high);
-        return (i + 1);
-    }
-
-    static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int pi = partition(arr, low, high);
-            quickSort(arr, low, pi - 1);
-            quickSort(arr, pi + 1, high);
-        }
     }
 
     static void printArray(int[] arr, int size) {
@@ -34,7 +46,7 @@ public class quickSort {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 10, 7, 8, 9, 1, 5, 4, 2 };
+        int[] arr = {11, 11, 12, 16, 5, 5, 4, 1, 2, 0, 99, 56, 75, 23, 16, 17, 18};
 
         quickSort(arr, 0, arr.length - 1);
         System.out.println("Sorted array: ");
